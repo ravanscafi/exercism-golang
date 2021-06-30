f2911d9ae308c28dc708d08868d33010b0ea674b
Ravan Scafi <rscafi@gmail.com>
2021-06-30 18:19:27 -0300

    Solve ETL

--- etl/etl.go
+++ etl/etl.go
@@ -1,1 +1,15 @@
 package etl
+
+import "strings"
+
+func Transform(m map[int][]string) map[string]int {
+	result := make(map[string]int)
+
+	for value, letters := range m {
+		for _, letter := range letters {
+			result[strings.ToLower(letter)] = value
+		}
+	}
+
+	return result
+}
