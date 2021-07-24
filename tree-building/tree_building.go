3d5ea0edebfc26a6edcc8c48abedc35eb68e7f8c
Ravan Scafi <rscafi@gmail.com>
2021-07-24 15:03:54 -0300

    Solve Tree Building

--- tree-building/tree_building.go
+++ tree-building/tree_building.go
@@ -1,5 +1,10 @@
 package tree
 
+import (
+	"errors"
+	"sort"
+)
+
 type Record struct {
 	ID,
 	Parent int
@@ -10,6 +15,29 @@ type Node struct {
 	Children []*Node
 }
 
-func Build(records []Record) (*Node, error) {
-	return nil, nil
+func Build(recs []Record) (*Node, error) {
+	nodes := make(map[int]*Node, len(recs))
+
+	sort.Slice(recs, func(i, j int) bool {
+		return recs[i].ID < recs[j].ID
+	})
+
+	for i, r := range recs {
+		if r.ID != i || r.Parent > i || i > 0 && r.Parent == i {
+			return nil, errors.New("invalid tree")
+		}
+
+		nodes[i] = &Node{ID: i}
+
+		if i == 0 {
+			continue
+		}
+
+		nodes[r.Parent].Children = append(
+			nodes[r.Parent].Children,
+			nodes[i],
+		)
+	}
+
+	return nodes[0], nil
 }
